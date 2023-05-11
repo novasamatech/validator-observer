cc9a92f0741e3e9960c6530b03bf2b969fdd049b
Stepan Lavrentev <lawrentievsv@gmail.com>
2023-05-11 07:49:40 +0300

    add waiting for transaction finalisation

--- src/PayoutHelper.ts
+++ src/PayoutHelper.ts
@@ -63,17 +63,15 @@ export class PayoutHelper {
     }
 
     private async sendTransaction(transaction: any, sender: any): Promise<void> {
-        await new Promise(async (resolve) => {
-            console.log(sender.address)
+        await new Promise(async (unsub) => {
             transaction.signAndSend(sender, ({ status, events }) => {
-                if (status.isInBlock || status.isFinalized) {
-                    events
-                        // find/filter for failed events
-                        .filter(({ event }) =>
-                            this.api.events.system.ExtrinsicFailed.is(event)
-                        )
-                        // we know that data for system.ExtrinsicFailed is
-                        // (DispatchError, DispatchInfo)
+                console.log(`Current status is $.status}`);
+
+                if (status.isInBlock) {
+                    console.log(`Transaction included at blockHash ${status.asInBlock}`);
+                    events.filter(({ event }) =>
+                        this.api.events.system.ExtrinsicFailed.is(event)
+                    )
                         .forEach(({ event: { data: [error, info] } }) => {
                             if (error.isModule) {
                                 // for module errors, we have the section indexed, lookup
@@ -81,16 +79,19 @@ export class PayoutHelper {
                                 const { docs, method, section } = decoded;
 
                                 console.log(`${section}.${method}: ${docs.join(' ')}`);
-                                resolve(info)
+                                unsub(info)
                             } else {
                                 // Other, CannotLookup, BadOrigin, no extra info
                                 console.log(error.toString());
-                                resolve(error)
+                                unsub(error)
                             }
                         });
+                } else if (status.isFinalized) {
+                    console.log(`Transaction finalized at blockHash ${status.asFinalized}`);
+                    unsub(status);
                 }
                 else {
-                    resolve(status)
+                    console.log(`Waiting for status update... Current status is: ${status}`);
                 }
             });
         })
