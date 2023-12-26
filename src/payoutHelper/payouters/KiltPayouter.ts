2a723623221053067154f78181a7eb41219ef4c8
Stepan Lavrentev <lawrentievsv@gmail.com>
2023-12-26 11:34:48 +0300

    clean up

--- src/payoutHelper/payouters/KiltPayouter.ts
+++ src/payoutHelper/payouters/KiltPayouter.ts
@@ -15,20 +15,40 @@ export class KiltPayoutHelper extends PayoutHelper {
      */
     async payoutRewards(validators: string[], sender, depth: boolean = false): Promise<void> {
         for (const validator of validators) {
-            const incrementCollatorRewards = this.api.tx.parachainStaking.incrementCollatorRewards()
-            const proxyIncrementTransact = this.api.tx.proxy.proxy(
-                validator,
-                'ParachainStaking',
-                incrementCollatorRewards
-            )
-            await sendTransaction(proxyIncrementTransact, sender, this.api)
-            const claimReward = this.api.tx.parachainStaking.claimRewards()
-            const proxyclaimTransact = this.api.tx.proxy.proxy(
-                validator,
-                'ParachainStaking',
-                claimReward,
-            )
-            await sendTransaction(proxyclaimTransact, sender, this.api)
+            await this.incrementCollatorRewards(validator, sender);
+            await this.claimRewards(validator, sender);
         }
+    }
+
+    /**
+     * Increments the rewards for a given validator.
+     * @param validator - The validator to increment rewards for.
+     * @param sender - The sender of the transaction.
+     * @returns A promise that resolves when the rewards have been incremented.
+     */
+    private async incrementCollatorRewards(validator: string, sender: any): Promise<void> {
+        const incrementCollatorRewards = this.api.tx.parachainStaking.incrementCollatorRewards();
+        const proxyIncrementTransact = this.api.tx.proxy.proxy(
+            validator,
+            'ParachainStaking',
+            incrementCollatorRewards
+        );
+        await sendTransaction(proxyIncrementTransact, sender, this.api);
+    }
+
+    /**
+     * Claims the rewards for a given validator.
+     * @param validator - The validator to claim rewards for.
+     * @param sender - The sender of the transaction.
+     * @returns A promise that resolves when the rewards have been claimed.
+     */
+    private async claimRewards(validator: string, sender: any): Promise<void> {
+        const claimReward = this.api.tx.parachainStaking.claimRewards();
+        const proxyclaimTransact = this.api.tx.proxy.proxy(
+            validator,
+            'ParachainStaking',
+            claimReward,
+        );
+        await sendTransaction(proxyclaimTransact, sender, this.api);
     }
 }
