88a9e51bce50486af9dbd7971c601a4a1fee067d
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2024-03-19 11:14:16 +0300

    Fix/astar_payout (#8)
    * fix first implementation
    
    * chore
    
    * simplify getDappAddress
    
    * remove unnecessary log

--- src/payoutHelper/payouters/AstarPayouter.ts
+++ src/payoutHelper/payouters/AstarPayouter.ts
@@ -6,7 +6,7 @@ import { PayoutHelper } from "../PayoutHelper";
  * AstarPayoutHelper is a class that extends PayoutHelper and provides methods to handle payouts on the Astar network.
  */
 export class AstarPayoutHelper extends PayoutHelper {
-    
+
     /**
      * Converts an era number to a u32 type.
      * @param era - The era number.
@@ -17,12 +17,12 @@ export class AstarPayoutHelper extends PayoutHelper {
     }
 
     /**
-     * Creates an AstarRuntimeSmartContract type with the given validator address.
-     * @param validatorAddress - The validator address.
-     * @returns The AstarRuntimeSmartContract type.
+     * Creates an AstarPrimitivesDappStakingSmartContract type with the given validator address.
+     * @param dappAddress - The dapp address.
+     * @returns The AstarPrimitivesDappStakingSmartContract type.
      */
-    private astarRuntimeSmartContract(validatorAddress: string) {
-        return this.api.createType('AstarRuntimeSmartContract', {'Evm': validatorAddress, 'Wasm': ''});
+    private astarRuntimeSmartContract(dappAddress: string) {
+        return this.api.createType('AstarPrimitivesDappStakingSmartContract', { 'Evm': dappAddress });
     }
 
     /**
@@ -35,9 +35,8 @@ export class AstarPayoutHelper extends PayoutHelper {
     async payoutRewards(validators: Validator[], sender, depth: boolean = false): Promise<void> {
         for (const validator of validators) {
             const dappAddress = await this.getDAPPAddress(validator.address)
-            const currentEra = await this.getCurrentEra()
-            if (dappAddress){
-                const erasToPayout = await this.getErasToReward(dappAddress, currentEra)
+            if (dappAddress) {
+                const erasToPayout = await this.getErasToReward(dappAddress)
                 await this.processPayout(dappAddress, erasToPayout, sender)
             }
         }
@@ -49,77 +48,77 @@ export class AstarPayoutHelper extends PayoutHelper {
      * @returns A promise that resolves with the DAPP address or undefined if it doesn't exist.
      */
     private async getDAPPAddress(validatorAddress: string): Promise<string | undefined> {
-        const dappAccount = await this.retryApiCall(() => 
-            this.api.query.dappsStaking.registeredDevelopers(validatorAddress)
+        const dappAccounts = await this.retryApiCall(() =>
+            this.api.query.dappStaking.integratedDApps.entries()
         );
-        const data = dappAccount.toJSON();
-        if (typeof data === 'object' && data !== null && 'evm' in data) {
-            return data.evm?.toString();
+        for (const [key, value] of dappAccounts) {
+            const onchainValidatorAddress = value.toJSON() as { owner?: string };
+            if (onchainValidatorAddress.owner === validatorAddress) {
+                const serializedData = key.toHuman() as [{ Evm?: string }];
+                if ('Evm' in serializedData[0]) {
+                    return serializedData[0].Evm;
+                }
+            }
         }
         throw new Error('Unexpected data type');
     }
 
     /**
-     * Gets the current era.
-     * @returns A promise that resolves with the current era.
-     */
-    private async getCurrentEra(): Promise<string> {
-        return (await this.retryApiCall(() => this.api.query.dappsStaking.currentEra())).toString()
-    }
-
-    /**
-     * Gets the eras to reward for the given validator address and current era.
-     * @param validatorAddress - The validator address.
-     * @param currentEra - The current era.
+     * Gets the eras to reward for the given dapp.
+     * @param dappAddress - The dapp address.
      * @returns A promise that resolves with an array of eras to reward.
      */
-    private async getErasToReward(validatorAddress: string, currentEra: string): Promise<Array<string>> {
+    private async getErasToReward(dappAddress: string): Promise<Array<string>> {
         const rewardEras: Array<string> = [];
-        let era = Number(currentEra) - 1; // in order to not get from currentEra
-    
-        for (; era >= 0; era--) {
-            const contractEraStake = await this.retryApiCall(() => 
-                this.api.query.dappsStaking.contractEraStake(
-                    this.astarRuntimeSmartContract(validatorAddress),
-                    this.eraIndex(era)
-                )
-            );
-            const data = contractEraStake.toJSON();
-    
-            if (typeof data === 'object' && data !== null && 'contractRewardClaimed' in data) {
-                if (data.contractRewardClaimed === false) {
-                    rewardEras.push(era.toString());
-                } else if (data.contractRewardClaimed === true) {
-                    break;
-                }
+        const dappInfo = await this.api.query.dappStaking.integratedDApps({ Evm: dappAddress });
+        const ourDappId = (dappInfo as any).unwrap().id.toNumber();
+
+        const rewardsByEra = await this.api.query.dappStaking.dAppTiers.entries();
+
+        rewardsByEra.forEach(([era, tierRewards]) => {
+            if ((tierRewards as any).isNone) return;
+
+            const tierDapps = (tierRewards as any).unwrap().dapps;
+            const dapps: Array<{ dappId: number, tierId: number }> = [];
+            tierDapps.forEach((value, key) => {
+                dapps.push({
+                    dappId: key.toNumber(),
+                    tierId: value.toNumber(),
+                });
+            });
+
+            const ourUnclaimedTier = dapps.find(({ dappId }) => dappId === ourDappId);
+            if (ourUnclaimedTier) {
+                const eraHumanReadable = era.toHuman() as string;
+                rewardEras.push(eraHumanReadable[0]);
             }
-        }
-    
+        });
+
         return rewardEras;
     }
 
     /**
-     * Processes the payout for the given validator address and reward eras.
-     * @param validatorAddress - The validator address.
+     * Processes the payout for the given dapp address and reward eras.
+     * @param dappAddress - The dapp address.
      * @param rewardEras - The reward eras.
      * @param sender - The sender of the transaction.
      * @param batch_size - The size of the batch of transactions to send.
      * @returns A promise that resolves when the payout has been processed.
      */
-    private async processPayout(validatorAddress: string, rewardEras: Array<string>, sender: any, batch_size: number = 20) {
+    private async processPayout(dappAddress: string, rewardEras: Array<string>, sender: any, batch_size: number = 20) {
         const transactions: any = [];
         rewardEras = rewardEras.reverse();
-    
+
         for (let i = 0; i < rewardEras.length; i += batch_size) {
-            const batch = rewardEras.slice(i, i + batch_size).map(era => 
-                this.api.tx.dappsStaking.claimDapp(
-                    this.astarRuntimeSmartContract(validatorAddress),
+            const batch = rewardEras.slice(i, i + batch_size).map(era =>
+                this.api.tx.dappStaking.claimDappReward(
+                    this.astarRuntimeSmartContract(dappAddress),
                     this.eraIndex(Number(era))
                 )
             )
             transactions.push(this.api.tx.utility.batch(batch));
         }
-    
+
         for (const transaction of transactions) {
             await sendTransaction(transaction, sender, this.api);
         }
