b3bf8024b90f71dd24143b8121c4b4bc9f159943
Stepan Lavrentev <lawrentievsv@gmail.com>
2024-04-24 23:55:39 +0300

    refactoring

--- src/payoutHelper/payouters/RelaychainPayouter.ts
+++ src/payoutHelper/payouters/RelaychainPayouter.ts
@@ -1,13 +1,19 @@
-import { SubmittableExtrinsic } from "@polkadot/api/types";
 import { Validator } from "../../config/conf";
 import { sendTransaction } from "../../utils";
 import { PayoutHelper } from "../PayoutHelper";
+import { NewRelaychainPayoutLogic } from "./NewRelaychainPayoutLogic";
+import { SubstrateConnection } from "../../connection";
 
 /**
  * RelychainPayoutHelper is a class that extends PayoutHelper and provides methods to handle payouts on the Relychain network.
  */
 export class RelychainPayoutHelper extends PayoutHelper {
+    private newPayoutLogic: NewRelaychainPayoutLogic;
 
+    constructor(api: SubstrateConnection) {
+        super(api);
+        this.newPayoutLogic = new NewRelaychainPayoutLogic(api);
+    }
     /**
      * Payout rewards for the given validators.
      * @param validators - The validators to payout rewards for.
@@ -17,7 +23,7 @@ export class RelychainPayoutHelper extends PayoutHelper {
      */
     async payoutRewards(validators: Validator[], sender, depth: boolean = false): Promise<void> {
         if (this.api.query.staking.erasStakersOverview) {
-            await this.newPayoutRewards(validators, sender, depth);
+            await this.newPayoutLogic.payoutRewards(validators, sender, depth);
         } else {
             for (const validator of validators) {
                 const unclaimedPayouts = await this.checkPayouts(validator.address, depth);
@@ -26,59 +32,6 @@ export class RelychainPayoutHelper extends PayoutHelper {
                 }
             }
         }
-    }
-
-    /**
-     * New method to handle payouts if eraStakersOverview exists.
-     * @param validators - The validators to payout rewards for.
-     * @param sender - The sender of the transaction.
-     * @param depth - Whether to check the history for unclaimed rewards.
-     */
-    async newPayoutRewards(validators: Validator[], sender, depth: boolean): Promise<void> {
-        const currentEra = await this.getCurrentEraNumber();
-        const historyDepth = this.getHistoryDepth();
-        const startEra = Math.max(currentEra - historyDepth, 0);
-        const transactions: Array<SubmittableExtrinsic<any, any>> = [];
-    
-        for (let era = startEra; era < currentEra; era++) {
-            for (const validator of validators) {
-                const eraStakersOverview = (await this.api.query.staking.erasStakersOverview(era, validator.address)).toJSON() || {};
-                if (Object.keys(eraStakersOverview).length === 0) continue; // Skip if erasStakersOverview is empty
-
-                const claimedPages = (await this.api.query.staking.claimedRewards(era, validator.address)).toJSON();
-                // @ts-ignore
-                if (claimedPages!.length == 0) {
-                    const transaction = this.api.tx.staking.payoutStakers(validator.address, era);
-                    transactions.push(transaction);
-                }
-            }
-        }
-    
-        const batchSize = 4;
-        for (let i = 0; i < transactions.length; i += batchSize) {
-            const batchTransactions = transactions.slice(i, i + batchSize);
-            const batchTransaction = this.api.tx.utility.batch(batchTransactions);
-            await sendTransaction(batchTransaction, sender, this.api);
-        }
-    }
-
-    /**
-     * Retrieves the current era number from the blockchain.
-     * @returns {Promise<number>} The current era as a number.
-     */
-    private async getCurrentEraNumber(): Promise<number> {
-        const currentEra = await this.api.query.staking.currentEra();
-        // @ts-ignore
-        return currentEra.unwrap().toNumber();
-    }
-
-    /**
-     * Fetches the history depth value from the blockchain constants.
-     * @returns {number} The history depth as a number.
-     */
-    private getHistoryDepth(): number {
-        const depth = this.api.consts.staking.historyDepth.toString();
-        return Number(depth);
     }
 
     /**
